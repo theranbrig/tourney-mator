f93a937f929b2282bcb5a2c31ee512d275fc24c1
Theran Brigowatz <theran.brigowatz@gmail.com>
2019-08-22 09:53:45 +0900

    added user data

--- screens/CreateTournamentScreen.js
+++ screens/CreateTournamentScreen.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { View, Text } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
 import Layout from '../src/utilities/Layout';
+import { UserContext } from '../src/utilities/UserContext';
 
-const CreateTournamentScreen = props => (
-  <Layout title="Pools">
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Create Pool</Text>
-    </View>
-  </Layout>
-);
+const CreateTournamentScreen = props => {
+  const { userLoading, setUser, userData, user } = useContext(UserContext);
+  const [userState, setUserState] = useState(null);
+
+  return (
+    <Layout title="Pools">
+      <NavigationEvents
+        onDidFocus={payload => {
+          if (userLoading) return <Text>Loading...</Text>;
+          setUserState(user);
+        }}
+      />
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Create Pool</Text>
+        <Text>{userState.username}</Text>
+      </View>
+    </Layout>
+  );
+};
 
 export default CreateTournamentScreen;
