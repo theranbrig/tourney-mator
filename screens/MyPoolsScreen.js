e6d2069011366fd9e4412aeffac5ec4fa31902cc
Theran Brigowatz <theran.brigowatz@gmail.com>
2019-10-20 09:26:39 +0900

    added header to pools screen

--- screens/MyPoolsScreen.js
+++ screens/MyPoolsScreen.js
@@ -1,10 +1,22 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { View, TouchableOpacity, ScrollView, RefreshControl } from 'react-native';
-import { Button, Text, List, ListItem, Body, Right } from 'native-base';
+import {
+  Button,
+  Text,
+  List,
+  ListItem,
+  Body,
+  Right,
+  Header,
+  Title,
+  Subtitle,
+  Left,
+  Icon,
+} from 'native-base';
 import Layout from '../src/utilities/Layout';
 import BottomFooter from '../src/components/Footer';
 import { UserContext } from '../src/utilities/UserContext';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+// import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useMutation } from '@apollo/react-hooks';
 import { ACCEPT_REQUEST_MUTATION, DELETE_REQUEST_MUTATION } from '../src/utilities/Mutations';
 
@@ -45,74 +57,104 @@ const MyPoolsScreen = ({ history }) => {
   return (
     <>
       <ScrollView
-        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={() => onRefresh()} />}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={() => onRefresh()}
+            style={{ backgroundColor: '#fc3' }}
+          />
+        }
       >
-        {user.tournamentRequests ? (
-          <List style={{ marginTop: 40 }}>
-            {user.tournamentRequests.map(request => (
-              <ListItem
-                style={{ backgroundColor: '#fc3', width: '100%', height: 50, marginLeft: 0 }}
-                key={request.id}
-              >
-                <Body>
-                  <Text>{request.tournament.name}</Text>
-                </Body>
-                <Right style={{ flexDirection: 'row', marginRight: 10 }}>
-                  <Button
-                    onPress={() => {
-                      acceptRequest({
-                        variables: { id: request.id, tournamentId: request.tournament.id },
-                      });
-                    }}
-                    rounded
-                    bordered
-                    style={{
-                      margin: 5,
-                      borderColor: '#7a0019',
-                      borderWidth: 3,
-                      width: 40,
-                      height: 40,
-                      textAlign: 'center',
-                    }}
-                  >
-                    <Icon
-                      name='check-outline'
-                      style={{ fontSize: 27, color: '#7a0019', paddingLeft: 6 }}
-                    />
-                  </Button>
-                  <Button
-                    onPress={() => {
-                      deleteRequest({ variables: { id: request.id } });
-                    }}
-                    rounded
-                    bordered
-                    style={{
-                      margin: 5,
-                      borderColor: '#7a0019',
-                      borderWidth: 3,
-                      width: 40,
-                      height: 40,
-                      textAlign: 'center',
-                    }}
-                  >
-                    <Icon
-                      name='close-outline'
-                      style={{ fontSize: 27, color: '#7a0019', paddingLeft: 6 }}
-                    />
-                  </Button>
-                </Right>
-              </ListItem>
-            ))}
-          </List>
-        ) : (
-          <></>
-        )}
         <Layout title='Pools'>
+          <Header style={{ backgroundColor: '#fc3' }}>
+            <Left>
+              <Icon
+                style={{ color: '#7a0019' }}
+                type='FontAwesome5'
+                name='chevron-left'
+                onPress={() => history.goBack()}
+              />
+            </Left>
+            <Body>
+              <Title style={{ color: '#7a0019', fontFamily: 'graduate', fontSize: 20 }}>
+                MY POOLS
+              </Title>
+              <Subtitle style={{ color: '#7a0019', fontFamily: 'graduate', fontSize: 15 }}>
+                {user.username}
+              </Subtitle>
+            </Body>
+            <Right>
+              <Button transparent>
+                <Icon
+                  style={{ color: '#7a0019' }}
+                  type='FontAwesome5'
+                  name='plus-circle'
+                  onPress={() => history.push('/create')}
+                />
+              </Button>
+            </Right>
+          </Header>
+          {user.tournamentRequests ? (
+            <List style={{ marginTop: 40, backgroundColor: '#fc3' }}>
+              {user.tournamentRequests.map(request => (
+                <ListItem
+                  style={{ backgroundColor: '#fc3', width: '100%', height: 50, marginLeft: 0 }}
+                  key={request.id}
+                >
+                  <Body>
+                    <Text>{request.tournament.name}</Text>
+                  </Body>
+                  <Right style={{ flexDirection: 'row', marginRight: 10 }}>
+                    <Button
+                      onPress={() => {
+                        acceptRequest({
+                          variables: { id: request.id, tournamentId: request.tournament.id },
+                        });
+                      }}
+                      rounded
+                      bordered
+                      style={{
+                        margin: 5,
+                        borderColor: '#7a0019',
+                        borderWidth: 3,
+                        width: 40,
+                        height: 40,
+                        textAlign: 'center',
+                      }}
+                    >
+                      <Icon
+                        name='check-outline'
+                        style={{ fontSize: 27, color: '#7a0019', paddingLeft: 6 }}
+                      />
+                    </Button>
+                    <Button
+                      onPress={() => {
+                        deleteRequest({ variables: { id: request.id } });
+                      }}
+                      rounded
+                      bordered
+                      style={{
+                        margin: 5,
+                        borderColor: '#7a0019',
+                        borderWidth: 3,
+                        width: 40,
+                        height: 40,
+                        textAlign: 'center',
+                      }}
+                    >
+                      <Icon
+                        name='close-outline'
+                        style={{ fontSize: 27, color: '#7a0019', paddingLeft: 6 }}
+                      />
+                    </Button>
+                  </Right>
+                </ListItem>
+              ))}
+            </List>
+          ) : (
+            <></>
+          )}
           <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Pools Screen</Text>
-            <Button title='Create Tourney' onPress={() => history.push('/create')}>
-              <Text>Create</Text>
-            </Button>
             <Button title='Wait for Tourney' onPress={() => history.push('/waiting')}>
               <Text>Wait</Text>
             </Button>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Header, Button, Left, Body, Right, Icon, Text, Title } from 'native-base';
 
 const AppHeader = props => (
-  <Header style={{ backgroundColor: '#fc3' }} iosBarStyle="light-content">
+  <Header style={{ backgroundColor: '#fc3' }} iosBarStyle='light-content'>
     <Left>
       <Button style={{ backgroundColor: '#fc3' }} onPress={() => props.history.goBack()}>
-        <Icon style={{ color: '#7a0019' }} type="FontAwesome5" name="chevron-left" />
+        <Icon style={{ color: '#7a0019' }} type='FontAwesome5' name='chevron-left' />
       </Button>
     </Left>
     <Body style={{ textAlign: 'center' }}>
-      <Title style={{ color: '#7a0019', fontFamily: 'graduate' }}>{props.title}</Title>
+      <Title style={{ color: '#7a0019', fontFamily: 'graduate', fontSize: 18 }}>
+        {props.title}
+      </Title>
     </Body>
     <Right />
   </Header>
