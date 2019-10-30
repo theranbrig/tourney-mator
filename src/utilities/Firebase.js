1f5c922443f166cb990dab53033b0c375197cc15
Theran Brigowatz <theran.brigowatz@gmail.com>
2019-10-30 20:57:55 +0900

    added firebase basics

--- src/utilities/Firebase.js
+++ src/utilities/Firebase.js
@@ -2,10 +2,32 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: '<YOUR-API-KEY>',
-  authDomain: '<YOUR-AUTH-DOMAIN>',
-  databaseURL: '<YOUR-DATABASE-URL>',
-  storageBucket: '<YOUR-STORAGE-BUCKET>',
+  apiKey: 'AIzaSyD5xRHDREmUCdt9JhZpsxVC0cG0pWO8qZk',
+  authDomain: 'bracketball-83683.firebaseapp.com',
+  databaseURL: 'https://bracketball-83683.firebaseio.com',
+  projectId: 'bracketball-83683',
+  storageBucket: 'bracketball-83683.appspot.com',
+  messagingSenderId: '129929714791',
+  appId: '1:129929714791:web:49de46b1860dd8445f88c2',
+  measurementId: 'G-XC986894ET',
 };
-
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Firebase test.
+function storeHighScore(userId, score) {
+  firebase
+    .database()
+    .ref('users/' + userId)
+    .set({
+      highscore: score,
+    });
+}
+
+setupHighscoreListener(userId) {
+  firebase.database().ref('users/' + userId).on('value', (snapshot) => {
+    const highscore = snapshot.val().highscore;
+    console.log("New high score: " + highscore);
+  });
+}
