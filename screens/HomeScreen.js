d2edc6d48def7a5a0e85d39d6a5dc1f7be27d4dd
Theran <theran.brigowatz@gmail.com>
2019-12-30 12:52:25 +0900

    updated scroll

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,6 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React, { useContext, useState, useEffect } from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-  RefreshControl,
-} from 'react-native';
+import { Image, Platform, ScrollView, StyleSheet, TouchableOpacity, View, RefreshControl } from 'react-native';
 import { Content, Container, Footer, FooterTab, Button, Text, List, ListItem } from 'native-base';
 import { UserContext } from '../src/utilities/UserContext';
 import Layout from '../src/utilities/Layout';
@@ -88,9 +80,7 @@ const HomeScreen = ({ history }) => {
   });
 
   const getScores = params => {
-    fetch(
-      'http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard?limit=900'
-    )
+    fetch('http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard?limit=900')
       .then(res => res.json())
       .then(data => {
         setScheduleData(data);
@@ -108,9 +98,7 @@ const HomeScreen = ({ history }) => {
 
   useEffect(() => {
     setLoading(true);
-    fetch(
-      'http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard'
-    )
+    fetch('http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard')
       .then(res => res.json())
       .then(data => {
         setScheduleData(data);
@@ -127,10 +115,7 @@ const HomeScreen = ({ history }) => {
           <Text style={styles.title}>Loading Scores...</Text>
           <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
             <View style={{ height: 350 }}>
-              <Image
-                style={{ width: 300, height: 250 }}
-                source={require('../assets/images/goldBasketball.png')}
-              />
+              <Image style={{ width: 300, height: 250 }} source={require('../assets/images/goldBasketball.png')} />
             </View>
           </View>
         </View>
@@ -140,14 +125,10 @@ const HomeScreen = ({ history }) => {
 
   return (
     <>
-      <BackButtonHeader history={history} title={'HOME'} />
+      <BackButtonHeader history={history} title="HOME" />
       {scheduleData && (
-        <>
+        <View style={{ flex: 1, backgroundColor: '#710019' }}>
           <ScrollView
-            style={{ backgroundColor: '#7a0019' }}
-            contentContainerStyle={{
-              flex: 1,
-            }}
             refreshControl={
               <RefreshControl
                 refreshing={refreshing}
@@ -157,111 +138,85 @@ const HomeScreen = ({ history }) => {
               />
             }
           >
-            <Layout>
-              <Text
-                style={{
-                  textAlign: 'center',
-                  color: '#fc3',
-                  fontFamily: 'graduate',
-                  fontSize: 20,
-                  padding: 20,
-                }}
-              >
-                Today's {scheduleData.leagues[0].name} Scores
-              </Text>
-              <List style={{ marginLeft: 0 }}>
-                {scheduleData.events.map(event => (
-                  <ListItem
-                    key={event.id}
-                    style={{
-                      marginLeft: 0,
-                      backgroundColor: '#ffffff',
-                      height: 120,
-                      paddingLeft: 10,
-                      opacity: 0.9,
-                      borderColor: '#7a0019',
-                      width: '90%',
-                      marginBottom: 10,
-                      marginLeft: '5%',
-                      borderWidth: 3,
-                      borderColor: '#fc3',
-                    }}
-                  >
+            <Text
+              style={{
+                textAlign: 'center',
+                color: '#fc3',
+                fontFamily: 'graduate',
+                fontSize: 20,
+                padding: 20,
+              }}
+            >
+              Today's {scheduleData.leagues[0].name} Scores
+            </Text>
+            <List style={{ marginLeft: 0, backgroundColor: '#710019' }}>
+              {scheduleData.events.map(event => (
+                <ListItem
+                  key={event.id}
+                  style={{
+                    marginLeft: 0,
+                    backgroundColor: '#ffffff',
+                    height: 120,
+                    paddingLeft: 10,
+                    opacity: 0.9,
+                    borderColor: '#7a0019',
+                    width: '90%',
+                    marginBottom: 10,
+                    marginLeft: '5%',
+                    borderWidth: 3,
+                    borderColor: '#fc3',
+                  }}
+                >
+                  <View style={{ flex: 1, flexDirection: 'column' }}>
                     <View style={{ flex: 1, flexDirection: 'column' }}>
-                      <View style={{ flex: 1, flexDirection: 'column' }}>
-                        <View
-                          style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}
-                        >
-                          <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
-                            <Image
-                              style={{ width: 30, height: 30, marginRight: 10 }}
-                              source={{ uri: event.competitions[0].competitors[1].team.logo }}
-                            />
-                            {event.competitions[0].competitors[1].curatedRank.current < 26 && (
-                              <Text>
-                                ({event.competitions[0].competitors[1].curatedRank.current})
-                              </Text>
-                            )}
-                            <Text
-                              style={
-                                event.competitions[0].competitors[1].winner && { color: '#7a0019' }
-                              }
-                            >
-                              {event.competitions[0].competitors[1].team.shortDisplayName} (
-                              {event.competitions[0].competitors[1].records[0].summary})
-                            </Text>
-                          </View>
-
-                          <Text
-                            style={
-                              event.competitions[0].competitors[1].winner && { color: '#7a0019' }
-                            }
-                          >
-                            {event.competitions[0].competitors[1].score}
+                      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+                          <Image
+                            style={{ width: 30, height: 30, marginRight: 10 }}
+                            source={{ uri: event.competitions[0].competitors[1].team.logo }}
+                          />
+                          {event.competitions[0].competitors[1].curatedRank.current < 26 && (
+                            <Text>({event.competitions[0].competitors[1].curatedRank.current})</Text>
+                          )}
+                          <Text style={event.competitions[0].competitors[1].winner && { color: '#7a0019' }}>
+                            {event.competitions[0].competitors[1].team.shortDisplayName} (
+                            {event.competitions[0].competitors[1].records[0].summary})
                           </Text>
                         </View>
-                        <View
-                          style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}
-                        >
-                          <View style={{ flex: 1, flexDirection: 'row' }}>
-                            <Image
-                              style={{ width: 30, height: 30, marginRight: 10 }}
-                              source={{ uri: event.competitions[0].competitors[0].team.logo }}
-                            />
-                            {event.competitions[0].competitors[0].curatedRank.current < 26 && (
-                              <Text>
-                                ({event.competitions[0].competitors[0].curatedRank.current})
-                              </Text>
-                            )}
-                            <Text
-                              style={
-                                event.competitions[0].competitors[0].winner && { color: '#7a0019' }
-                              }
-                            >
-                              {event.competitions[0].competitors[0].team.shortDisplayName} (
-                              {event.competitions[0].competitors[0].records[0].summary})
-                            </Text>
-                          </View>
 
-                          <Text
-                            style={
-                              event.competitions[0].competitors[0].winner && { color: '#7a0019' }
-                            }
-                          >
-                            {event.competitions[0].competitors[0].score}
+                        <Text style={event.competitions[0].competitors[1].winner && { color: '#7a0019' }}>
+                          {event.competitions[0].competitors[1].score}
+                        </Text>
+                      </View>
+                      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <View style={{ flex: 1, flexDirection: 'row' }}>
+                          <Image
+                            style={{ width: 30, height: 30, marginRight: 10 }}
+                            source={{ uri: event.competitions[0].competitors[0].team.logo }}
+                          />
+                          {event.competitions[0].competitors[0].curatedRank.current < 26 && (
+                            <Text>({event.competitions[0].competitors[0].curatedRank.current})</Text>
+                          )}
+                          <Text style={event.competitions[0].competitors[0].winner && { color: '#7a0019' }}>
+                            {event.competitions[0].competitors[0].team.shortDisplayName} (
+                            {event.competitions[0].competitors[0].records[0].summary})
                           </Text>
                         </View>
-                        <View>
-                          <Text style={{ alignSelf: 'flex-end' }}>{event.status.type.detail}</Text>
-                        </View>
+
+                        <Text style={event.competitions[0].competitors[0].winner && { color: '#7a0019' }}>
+                          {event.competitions[0].competitors[0].score}
+                        </Text>
+                      </View>
+                      <View>
+                        <Text style={{ alignSelf: 'flex-end' }}>{event.status.type.detail}</Text>
                       </View>
                     </View>
-                  </ListItem>
-                ))}
-              </List>
-            </Layout>
+                  </View>
+                </ListItem>
+              ))}
+            </List>
           </ScrollView>
-        </>
+        </View>
       )}
       <BottomFooter history={history} />
     </>
